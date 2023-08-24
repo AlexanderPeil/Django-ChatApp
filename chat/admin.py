5654df981263ab6bdd3596a942651faa48a32280
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2023-08-24 11:20:18 +0200

    Added login

--- chat/admin.py
+++ chat/admin.py
@@ -2,8 +2,8 @@ from .models import Chat, Message
 from django.contrib import admin
 
 class MessageAdmin(admin.ModelAdmin):
-    fields = ('chat', 'text', 'created_at', 'author', 'receiver',)
-    list_display = ('text', 'created_at', 'author', 'receiver',)
+    fields = ('chat', 'text', 'created_at', 'author', 'receiver')
+    list_display = ('text', 'created_at', 'author', 'receiver')
     search_fields = ('text',)
 
 # Register your models here.
