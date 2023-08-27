469c6f79217cd11915f1ddbcaf045e24db58ac81
Alexander Peil <121516867+SaschkaSCF@users.noreply.github.com>
2023-08-27 13:15:52 +0200

    Added templates  and static folder

--- chat/views.py
+++ chat/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from .models import Chat, Message
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 
+
+@login_required(login_url='/login/')
 def index(request):
     if request.method == 'POST':
         print('Received data ' + request.POST['textmessage'])
@@ -12,13 +15,15 @@ def index(request):
     chatMessages = Message.objects.filter(chat__id=1)
     return render(request, 'chat/index.html', {'messages' : chatMessages})
 
+
 def login_view(request):
+    redirect = request.GET.get('next')
     if request.method == 'POST':
         user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
 
         if user:
             login(request, user)
-            return HttpResponseRedirect('/chat/')
+            return HttpResponseRedirect(request.POST.get('redirect'))
         else:
-             return render(request, 'auth/login.html', {'wrongPassword': True})
-    return render(request, 'auth/login.html')
+             return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
+    return render(request, 'auth/login.html', {'redirect': redirect})
